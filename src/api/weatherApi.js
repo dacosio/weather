6554f679de086cc6992b4da83f439fc86c6c3602
Don Cosio <dacosio@gmail.com>
2023-10-23 22:29:59 -0700

    removed api key

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,5 +1,4 @@
 const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-const WEATHER_API_KEY = "e9d9c9f61e8b2720e85d08d734abbb62";
 
 export async function fetchWeatherData(lat, lon) {
   try {
